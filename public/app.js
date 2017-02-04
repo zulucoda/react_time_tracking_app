2f4d91df60e0509dfb6aa6c2de9e79154624511e
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2017-02-04 12:04:43 +0200

    adding events to handle delete timer, my solution :boom: :boom:

--- public/app.js
+++ public/app.js
@@ -42,11 +42,25 @@ const TimersDashboard = React.createClass({
       })
     })
   },
+  handleDelete: function (timerId) {
+    this.deleteTimer(timerId);
+  },
+  deleteTimer: function (timerId) {
+    this.setState({
+      timers: this.state.timers.filter((timer) =>{
+        return timer.id !== timerId;
+      })
+    });
+  },
   render: function () {
     return (
       <div className="ui three column centered grid">
         <div className="column">
-          <EditableTimerList timers={this.state.timers} onFormSubmit={this.handleEditFormSubmit} />
+          <EditableTimerList
+            timers={this.state.timers}
+            onFormSubmit={this.handleEditFormSubmit}
+            onDeleteTimer={this.handleDelete}
+          />
           <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
         </div>
       </div>
@@ -65,6 +79,7 @@ const EditableTimerList = React.createClass({
         elapsed={timer.elapsed}
         runningSince={timer.runningSince}
         onFormSubmit={this.props.onFormSubmit}
+        onDeleteTimer={this.props.onDeleteTimer}
       />
     ));
     return (
@@ -90,6 +105,9 @@ const EditableTimer = React.createClass({
   handleSubmit: function (timer) {
     this.props.onFormSubmit(timer);
     this.closeForm();
+  },
+  handleTimerDelete: function (timeId) {
+    this.props.onDeleteTimer(timeId);
   },
   closeForm : function () {
     this.setState({ editFormOpen: false });
@@ -117,6 +135,7 @@ const EditableTimer = React.createClass({
           elapsed={this.props.elapsed}
           runningSince={this.props.runningSince}
           onEditClick={this.handleEditClick}
+          onDeleteClick={this.handleTimerDelete}
         />
       );
     }
@@ -196,6 +215,9 @@ const ToggleableTimerForm = React.createClass({
 });
 
 const Timer = React.createClass({
+  handleOnDeleteClick: function () {
+    this.props.onDeleteClick(this.props.id);
+  },
   render: function () {
     const elapsedString = helpers.renderElapsedString(this.props.elapsed);
     return (
@@ -210,7 +232,9 @@ const Timer = React.createClass({
             <span className="right floated edit icon" onClick={this.props.onEditClick}>
               <i className="edit icon"></i>
             </span>
-            <span className="right floated trash icon"><i className="trash icon"></i></span>
+            <span className="right floated trash icon" onClick={this.handleOnDeleteClick}>
+              <i className="trash icon"></i>
+            </span>
           </div>
         </div>
         <div className="ui bottom attached blue basic button">
